a1ab516eba805973b64f69c12a7248a8c2e90347
maimai-y <maimai.ta.0505@gmail.com>
2020-10-24 11:39:22 +0900

    correct .gitignore

---
+++ parser.mli
@@ -0,0 +1,22 @@
+type token =
+  | NUMBER of (int)
+  | VAR of (string)
+  | PLUS
+  | MINUS
+  | TIMES
+  | TRUE
+  | FALSE
+  | EQUAL
+  | LESS
+  | GREATER
+  | LPAREN
+  | RPAREN
+  | IF
+  | THEN
+  | ELSE
+  | LET
+  | IN
+  | EOF
+
+val start :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Syntax.t
